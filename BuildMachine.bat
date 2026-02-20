6c56d9c4c69590ae979539055fad9241a062cd69
xiaprojects <github@xiaprojects.com>
2026-02-20 20:40:58 +0100

    Road to BSP #119

--- BuildMachine.bat
+++ BuildMachine.bat
@@ -2,6 +2,20 @@
 for /f "delims=" %%i in ('git describe --tags') do set VERSION=%%i
 md build\RB-%VERSION%
 
+set "TEMPLATE=TOUCH-30hz-LIGHT-2.1"
+copy /Y main\RB\BuildMachine-Template-RB-02-%TEMPLATE%.h main\RB\BuildMachine.h
+echo #define RB_VERSION "%VERSION%" >> main\RB\BuildMachine.h
+idf.py app
+esptool --chip esp32s3 merge_bin -o build\RB-%VERSION%\RB-02-%VERSION%-%TEMPLATE%.bin --flash_mode dio --flash_freq 80m --flash_size 16MB 0x0000 .\build\bootloader\bootloader.bin 0x8000 .\build\partition_table\partition-table.bin 0x10000 .\build\rb_project.bin
+del build\rb_project.bin
+
+set "TEMPLATE=TOUCH-30hz-LIGHT-2.8"
+copy /Y main\RB\BuildMachine-Template-RB-02-%TEMPLATE%.h main\RB\BuildMachine.h
+echo #define RB_VERSION "%VERSION%" >> main\RB\BuildMachine.h
+idf.py app
+esptool --chip esp32s3 merge_bin -o build\RB-%VERSION%\RB-02-%VERSION%-%TEMPLATE%.bin --flash_mode dio --flash_freq 80m --flash_size 16MB 0x0000 .\build\bootloader\bootloader.bin 0x8000 .\build\partition_table\partition-table.bin 0x10000 .\build\rb_project.bin
+del build\rb_project.bin
+
 set "TEMPLATE=NONTOUCH-30hz-GPS-2.8-AAT"
 copy /Y main\RB\BuildMachine-Template-RB-02-%TEMPLATE%.h main\RB\BuildMachine.h
 echo #define RB_VERSION "%VERSION%" >> main\RB\BuildMachine.h
